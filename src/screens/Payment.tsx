31123a063185e3c731c8eebd91fce4f92971b662
Karanpreet Singh <karanx7230@gmail.com>
2026-08-11 15:33:28 +0530

    feat: Add Visa card image and update payment screen to use it

--- src/screens/Payment.tsx
+++ src/screens/Payment.tsx
@@ -83,7 +83,7 @@ export default function Payment({ navigation }: any) {
         </View>
 
         <Image
-          source={require('../assets/images/Card.png')}
+          source={require('../assets/images/cardVisa.png')}
           resizeMode="contain"
           style={styles.imagecard}
         />
