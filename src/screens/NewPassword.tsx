78200a44d0ed875e612fa8583a3cc00b775ddaa6
Karanpreet Singh <karanx7230@gmail.com>
2026-08-11 15:24:15 +0530

    feat: Update navigation flow to replace login screen after password confirmation

--- src/screens/NewPassword.tsx
+++ src/screens/NewPassword.tsx
@@ -34,7 +34,7 @@ export default function NewPassword({ navigation }: any) {
         setErrorMessage('Passwords do not match!');
       } else {
         setErrorMessage('');
-        navigation.navigate(Routes.MAIN_TABS);
+        navigation.replace(Routes.LOGIN);
       }
     }
   };
